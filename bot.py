2b717e0c07dd908d97b6768831b1c50ab8cb6846
TESTEW1 <wagnersantos.apg@gmail.com>
2026-02-12 18:54:34 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ CANAL_CHAT_STAFF_GERAL = "🔰・chat-staff"
 CANAL_RANKING_MONSTRINHO = "🎰・ranking-monstrinho"
 CANAL_LOJA_INFO = "💾・loja-monstrinho"
 CANAL_DIRECAO = "👑・chat-direção"
-
+CANAL_ATENÇÃO = "⚠️・atenção"
 # GIFs e Imagens
 BANNER_TICKET = "https://i.pinimg.com/originals/5d/92/5d/5d925dd101dba34f341148eace3cfe38.gif"
 GIF_NAMORADOS = "https://i.pinimg.com/originals/f5/b8/44/f5b844675a7942e4180bb9960c3fe319.gif"
